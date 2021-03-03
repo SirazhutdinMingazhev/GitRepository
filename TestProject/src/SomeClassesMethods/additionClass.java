1478b9594d970bd601473a376d00f05a45f13fee
SirazhutdinMingazhev <selenium.test.sirazh@gmail.com>
2021-03-03 11:20:03 -0500

    third commit

--- TestProject/src/SomeClassesMethods/additionClass.java
+++ TestProject/src/SomeClassesMethods/additionClass.java
@@ -5,8 +5,9 @@ public class additionClass {
 	public static void main(String[] args) {
 		int a=5;
 		int b=3;
-		int c=a*b;
+		int c=a+b;
 		System.out.println(c);
 	}
-
+//added comment 
+	//added comment for git hub
 }
